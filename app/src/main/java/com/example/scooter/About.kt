e703fcbb39f8130835939a7a1f01d02c43c50074
ghettodimasta <dima_pavlov1702@mail.ru>
2023-03-14 19:02:25 +0300

    Added SideBar drawer + About us Activity

---
+++ app/src/main/java/com/example/scooter/About.kt
@@ -0,0 +1,3 @@
+package com.example.scooter
+
+class About : Sidebar(layout = R.layout.activity_about) {}
--- app/src/main/java/com/example/scooter/MainActivity.kt
+++ app/src/main/java/com/example/scooter/MainActivity.kt
@@ -9,10 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonClick = findViewById<Button>(R.id.signup_button)
-        buttonClick.setOnClickListener {
+        val buttonClickSignUp = findViewById<Button>(R.id.signup_button)
+        buttonClickSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        val buttonClickSignIn = findViewById<Button>(R.id.signin_button)
+        buttonClickSignIn.setOnClickListener {
+            val intent = Intent(this, Drawler::class.java)
+            startActivity(intent) }
     }
 }
