7798461bacbcacf8c1d62b06200d4b2e55c3e2c9
bskvorchevsky <s.bogdan007@yandex.ru>
2023-03-14 15:13:13 +0300

    sign up activity

--- app/src/main/java/com/example/scooter/MainActivity.kt
+++ app/src/main/java/com/example/scooter/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.scooter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonClick = findViewById<Button>(R.id.signup_button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
