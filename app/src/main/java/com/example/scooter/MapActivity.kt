1df70f4f0658aebcf2c6b1b426a4ff9af553f72e
bskvorchevsky <s.bogdan007@yandex.ru>
2023-03-16 16:17:12 +0300

    map activity finished

--- app/src/main/java/com/example/scooter/MapActivity.kt
+++ app/src/main/java/com/example/scooter/MapActivity.kt
@@ -7,10 +7,14 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.view.isVisible
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.GeoObject
 import com.yandex.mapkit.MapKitFactory
@@ -18,6 +22,8 @@ import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.layers.GeoObjectTapEvent
 import com.yandex.mapkit.layers.GeoObjectTapListener
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObject
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.runtime.image.ImageProvider
 import io.github.cdimascio.dotenv.dotenv
@@ -31,34 +37,30 @@ class MapActivity : AppCompatActivity() {
         Point(59.970634, 30.319071),
         Point(59.971843, 30.319832),
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dotenv = dotenv {
-            directory = "/assets"
-            filename = "env"
-        }
-        MapKitFactory.setApiKey(dotenv["YANDEX_MAPS_API_KEY"])
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_map)
         this.mapview = findViewById<View>(R.id.mapview) as MapView
         mapview.getMap().move(
             CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 5f),
+            Animation(Animation.Type.LINEAR, 1f),
             null
         )
         for (location in scooter_location_list) {
-            mapview.map.mapObjects.addPlacemark(
+            val mapObj = mapview.map.mapObjects.addPlacemark(
                 location, ImageProvider.fromResource(
                     this, R.drawable.kick_scooter
                 )
-            ).addTapListener { _, _ ->
-                Log.d("Marker", "Latitude: ${location.latitude}, Longitude: ${location.longitude}")
-                Toast.makeText(this, "Latitude: ${location.latitude}, Longitude: ${location.longitude}", Toast.LENGTH_LONG).show()
+            )
+            mapObj.zIndex = 1f
+            mapObj.addTapListener { obj, point ->
+                onScooterClick(obj, point)
                 true
             }
         }
     }
-
 
     override fun onStop() {
         mapview.onStop()
@@ -70,5 +72,27 @@ class MapActivity : AppCompatActivity() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
         mapview.onStart()
+    }
+
+    private fun onScooterClick(mapObject: MapObject, point: Point): Boolean {
+        val scooterInfo = findViewById<View>(R.id.scooter_info) as LinearLayout
+        scooterInfo.isVisible = true
+        val startButton = findViewById<View>(R.id.start_button)
+        val stopButton = findViewById<View>(R.id.stop_button)
+        startButton.setOnClickListener {
+            findViewById<View>(R.id.scooter_info_text_start).isVisible = false
+            findViewById<View>(R.id.scooter_info_text_stop).isVisible = true
+            startButton.isVisible = false
+            stopButton.isVisible = true
+        }
+        stopButton.setOnClickListener {
+            findViewById<View>(R.id.scooter_info_text_start).isVisible = true
+            findViewById<View>(R.id.scooter_info_text_stop).isVisible = false
+            startButton.isVisible = true
+            stopButton.isVisible = false
+            scooterInfo.isVisible = false
+        }
+        Log.d("Marker", point.toString())
+        return true
     }
 }
