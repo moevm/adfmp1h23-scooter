70c2b5d1901573c940ade4603b0724e874543e9b
ghettodimasta <dima_pavlov1702@mail.ru>
2023-03-17 15:49:33 +0300

    Finished Payment Details fragment

--- app/src/main/java/com/example/scooter/PaymentDetailsFragment.kt
+++ app/src/main/java/com/example/scooter/PaymentDetailsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.scooter
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.text.TextWatcher
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +32,42 @@ class PaymentDetailsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment_details, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_payment_details, container, false)
+
+        // Get a reference to the expiration date EditText view
+        val expirationDateEditText: EditText = view.findViewById(R.id.expirationDateEditText)
+
+        // Add a TextWatcher to the expiration date EditText view
+        expirationDateEditText.addTextChangedListener(object : TextWatcher {
+            private var isFormatting = false
+            private var slashInserted = false
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // If the user typed in two digits and a slash hasn't been inserted yet
+                if (!isFormatting && !slashInserted && s?.length == 2 && count == 1) {
+                    isFormatting = true
+                    slashInserted = true
+                    expirationDateEditText.setText(String.format("%s/", s))
+                    expirationDateEditText.setSelection(expirationDateEditText.text.length)
+                } else if (!isFormatting && slashInserted && s?.length == 2 && count == 0) {
+                    isFormatting = true
+                    slashInserted = false
+                    expirationDateEditText.setText(s.subSequence(0, s.length - 1))
+                    expirationDateEditText.setSelection(expirationDateEditText.text.length)
+                } else {
+                    slashInserted = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                isFormatting = false
+            }
+        })
+
+        return view
     }
 
     companion object {
