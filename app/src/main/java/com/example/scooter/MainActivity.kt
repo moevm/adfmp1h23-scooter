e85b2b5ef5b3bc5e9f04e98dd63a0ec62003c5c2
Katerzar <54950459+Katerzar@users.noreply.github.com>
2023-03-26 21:25:49 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/scooter/MainActivity.kt
+++ app/src/main/java/com/example/scooter/MainActivity.kt
@@ -4,20 +4,30 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
 import com.yandex.mapkit.MapKitFactory
 import io.github.cdimascio.dotenv.dotenv
 
 class MainActivity : AppCompatActivity() {
+
+    val signInViewModel: SignInViewModel by viewModels()
+
+    lateinit var loginEt: EditText
+    lateinit var passwordEt: EditText
+
     @SuppressLint("LongLogTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginEt = findViewById(R.id.username_edittext)
+        passwordEt = findViewById(R.id.password_edittext)
         val dotenv = dotenv {
             directory = "/assets"
-            filename = "env"
+            filename = "env.template"
         }
         try {
             MapKitFactory.setApiKey(dotenv["YANDEX_MAPS_API_KEY"])
@@ -32,8 +42,17 @@ class MainActivity : AppCompatActivity() {
         }
         val loginBtn = findViewById<Button>(R.id.signin_button)
         loginBtn.setOnClickListener {
-            val intent = Intent(this, Sidebar::class.java)
-            startActivity(intent)
+            val email = loginEt.text.toString()
+            val password = passwordEt.text.toString()
+            val successfullyLogined = signInViewModel.loginUser(email, password)
+
+            if (successfullyLogined) {
+                Log.d("mytag", "Login successfull")
+                val intent = Intent(this, Sidebar::class.java)
+                startActivity(intent)
+            } else {
+                Log.d("mytag", "Can not login")
+            }
         }
     }
 }
