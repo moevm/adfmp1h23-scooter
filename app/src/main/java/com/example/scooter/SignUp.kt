583e0b3f5eb692d81d26c7a37cf431241937fab5
Katerzar <54950459+Katerzar@users.noreply.github.com>
2023-03-26 21:21:27 +0300

    Update SignUp.kt

--- app/src/main/java/com/example/scooter/SignUp.kt
+++ app/src/main/java/com/example/scooter/SignUp.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.widget.Button
 
 class SignUp : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         val buttonClick = findViewById<Button>(R.id.continue_button)
+
         buttonClick.setOnClickListener {
             val intent = Intent(this, Sidebar::class.java)
             startActivity(intent)
